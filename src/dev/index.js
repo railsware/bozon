78ca32d3ff2dc1cdf66aae4a76158a12f535e2c3
Alex Chaplinsky <alchaplinsky@gmail.com>
2020-03-31 20:08:31 +0200

    Cleanup dev script

--- src/dev/index.js
+++ src/dev/index.js
@@ -1,23 +1,11 @@
 import { app } from 'electron'
 import fs from 'fs'
 import path from 'path'
-import { spawn } from 'child_process'
 
 const browserWindows = {}
-const electron = path.join(__dirname, '..', '..', '..', 'node_modules', '.bin', 'electron')
 
 const reloadRenderer = () => {
   Object.values(browserWindows).forEach(window => window.webContents.reloadIgnoringCache())
-}
-
-// eslint-disable-next-line no-unused-vars
-const _reloadMain = () => {
-  const child = spawn(electron, [app.getAppPath()], {
-    detached: true,
-    stdio: 'inherit'
-  })
-  child.unref()
-  if (app) app.quit()
 }
 
 app.on('browser-window-created', (_, bw) => {
@@ -28,7 +16,3 @@ app.on('browser-window-created', (_, bw) => {
 })
 
 fs.watch(path.resolve(__dirname, '..', 'renderer'), {}, reloadRenderer)
-
-// Disable main process reloading for now
-// fs.watch(path.resolve(__dirname, '..', 'preload'), {}, reloadMain)
-// fs.watch(path.resolve(__dirname), {}, reloadMain)
