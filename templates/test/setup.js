fd0ef76d6dcd6f6610d38b679ea188649491a92e
Alex Chaplinsky <alchaplinsky@gmail.com>
2020-05-30 00:22:46 +0200

    Support running test on windows

--- templates/test/setup.js
+++ templates/test/setup.js
@@ -4,11 +4,13 @@ const { Application } = require('spectron')
 const appPath = () => {
   switch (process.platform) {
     case 'darwin':
-      return path.join(__dirname, '..', '.tmp', 'mac', '<%= name%>.app', 'Contents', 'MacOS', '<%= name%>');
+      return path.join(__dirname, '..', '.tmp', 'mac', '<%= name%>.app', 'Contents', 'MacOS', '<%= name%>')
     case 'linux':
-      return path.join(__dirname, '..', '.tmp', 'linux', '<%= name%>');
+      return path.join(__dirname, '..', '.tmp', 'linux', '<%= name%>')
+    case 'win32':
+      return path.join(__dirname, '..', '.tmp', 'win-unpacked', '<%= name%>.exe')
     default:
-      throw Error('Unsupported platform');
+      throw Error(`Unsupported platform ${process.platform}`)
   }
 }
 global.app = new Application({ path: appPath() })
