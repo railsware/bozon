47d74091b111ce287b325e4a0a22f10c28db1c4b
Alex Chaplinsky <alchaplinsky@gmail.com>
2020-09-26 11:40:13 +0200

    Update version

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -12,7 +12,7 @@ describe('bozon cli', () => {
 
   describe('version', () => {
     it('sets current version', () => {
-      expect(commander.version).toHaveBeenCalledWith('1.1.0')
+      expect(commander.version).toHaveBeenCalledWith('1.1.1')
     })
 
     it('sets usage instruction', () => {
