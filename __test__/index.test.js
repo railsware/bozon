fab1c17dba5bcec7f0be76f46a6e1fa94a3a4103
Alex Chaplinsky <alchaplinsky@gmail.com>
2021-03-26 17:52:42 +0100

    Version bump

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -12,7 +12,7 @@ describe('bozon cli', () => {
 
   describe('version', () => {
     it('sets current version', () => {
-      expect(commander.version).toHaveBeenCalledWith('1.2.0')
+      expect(commander.version).toHaveBeenCalledWith('1.2.1')
     })
 
     it('sets usage instruction', () => {
