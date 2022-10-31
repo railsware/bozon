e4d7276eb4df0c7297cbbf5448563a3c159b5ef0
Alex Chaplinsky <alchaplinsky@gmail.com>
2022-10-31 10:28:12 +0100

    Increase version

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -12,7 +12,7 @@ describe('bozon cli', () => {
 
   describe('version', () => {
     it('sets current version', () => {
-      expect(commander.version).toHaveBeenCalledWith('1.3.4')
+      expect(commander.version).toHaveBeenCalledWith('1.3.5')
     })
 
     it('sets usage instruction', () => {
